6768c01e5cf2f2c5540ef022b1bd6cdf3b02cc2d
nroldanf <nicolas@loka.com>
2022-05-17 12:33:32 -0500

    remove cdk v2 for now

--- infrastructure/app.py
+++
@@ -1,7 +0,0 @@
-from aws_cdk import App, Aws
-
-from stack import LokaFoldBasic
-
-app = App()
-LokaFoldBasic(app, "LokaFoldBasic")
-app.synth()
--- infrastructure/lokafold-app/chalicelib/__init__.py
+++
@@ -1,6 +0,0 @@
-from .utils import (
-    validate_input,
-    create_job_name,
-    upload_fasta_to_s3,
-    submit_batch_alphafold_job
-)
