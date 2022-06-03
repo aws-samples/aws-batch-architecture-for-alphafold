ec7eebdf4a3f5cf425a26df1cf795890ebd5aa38
Brian Loyal <bloyal@amazon.com>
2022-06-03 09:11:19 -0500

    Submit download jobs in parallel

--- notebooks/download_ref_data.py
+++ notebooks/download_ref_data.py
@@ -7,29 +7,31 @@ import argparse
 
 batch = boto3.client("batch")
 
+
 def _parse_args():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--stack_name', type=str, default=None)
-    parser.add_argument('--job_name', type=str, default="download_job")
-    parser.add_argument('--script', type=str, default="download_all_data.sh")
-    parser.add_argument('--cpu', type=int, default=4)
-    parser.add_argument('--memory', type=int, default=16)
-    parser.add_argument('--download_dir', type=str, default="/fsx")
-    parser.add_argument('--download_mode', type=str, default="reduced_dbs")
-    
+    parser.add_argument("--stack_name", type=str, default=None)
+    parser.add_argument("--job_name", type=str, default="download_job")
+    parser.add_argument("--script", type=str, default="all")
+    parser.add_argument("--cpu", type=int, default=4)
+    parser.add_argument("--memory", type=int, default=16)
+    parser.add_argument("--download_dir", type=str, default="/fsx")
+    parser.add_argument("--download_mode", type=str, default="reduced_dbs")
+
     return parser.parse_known_args()
+
 
 def submit_download_data_job(
     stack_name,
     job_name,
     script,
     cpu,
-    memory,    
+    memory,
     download_dir,
     download_mode,
-    ):
-    
+):
+
     if stack_name is None:
         stack_name = nbhelpers.list_alphafold_stacks()[0]["StackName"]
     batch_resources = nbhelpers.get_batch_resources(stack_name)
@@ -37,11 +39,7 @@ def submit_download_data_job(
     job_queue = batch_resources["download_job_queue"]
 
     container_overrides = {
-        "command": [
-            script,
-            download_dir,
-            download_mode         
-        ],
+        "command": [script, download_dir, download_mode],
         "resourceRequirements": [
             {"value": str(cpu), "type": "VCPU"},
             {"value": str(memory * 1000), "type": "MEMORY"},
@@ -57,18 +55,60 @@ def submit_download_data_job(
 
     return response
 
+
 if __name__ == "__main__":
 
     ### Command line parser
     args, _ = _parse_args()
 
-    response = submit_download_data_job(
-        stack_name = args.stack_name,
-        job_name = args.job_name,
-        script = args.script,
-        cpu = args.cpu,
-        memory = args.memory,        
-        download_dir = args.download_dir,
-        download_mode = args.download_mode
-    )
+    if args.script.upper() == "ALL":
+        all_scripts = [
+            "download_alphafold_params_s3.sh",
+            "download_bfd_s3.sh",
+            "download_mgnify_s3.sh",
+            "download_pdb70_s3.sh",
+            "download_pdb_mmcif_s3.sh",
+            "download_pdb_seqres_s3.sh",
+            "download_small_bfd_s3.sh",
+            "download_uniclust30_s3.sh",
+            "download_uniprot.sh",
+            "download_uniref90.sh",
+        ]
+
+        responses = []
+        for script in all_scripts:
+            script_response = submit_download_data_job(
+                stack_name=args.stack_name,
+                job_name=args.job_name + "-" + script,
+                script=script,
+                cpu=args.cpu,
+                memory=args.memory,
+                download_dir=args.download_dir,
+                download_mode=args.download_mode,
+            )
+            responses.append(script_response)
+        response = str(script_response)
+
+    elif args.script.upper() == "PARAMETERS_ONLY":
+        parameters_only_script = "download_alphafold_params_s3.sh"
+        response = submit_download_data_job(
+            stack_name=args.stack_name,
+            job_name=args.job_name,
+            script=parameters_only_script,
+            cpu=args.cpu,
+            memory=args.memory,
+            download_dir=args.download_dir,
+            download_mode=args.download_mode,
+        )
+
+    else:
+        response = submit_download_data_job(
+            stack_name=args.stack_name,
+            job_name=args.job_name,
+            script=args.script,
+            cpu=args.cpu,
+            memory=args.memory,
+            download_dir=args.download_dir,
+            download_mode=args.download_mode,
+        )
     print(response)
